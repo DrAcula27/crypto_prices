fe92480f0422ddbf76eee7b9ae2544ea6491b1ab
Danielle <danielle@kendallspace.net>
2023-02-07 14:59:35 -0500

    create Main, Currencies, Prices pages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// '/'           -> render Main Page
+// '/currencies' -> render Currencies Page
+// '/price'      -> render Price Page
+
 root.render(
   <React.StrictMode>
     <App />
