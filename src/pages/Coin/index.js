06b25ea0ecf4c7eda63dd3c5eab8d178e52681d9
Danielle <danielle@kendallspace.net>
2023-02-09 15:00:27 -0500

    setup coin and currency pages

--- src/pages/Coin/index.js
+++ src/pages/Coin/index.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Coin = () => {
-  return <div className="page-container">Coin</div>;
+  let { symbol } = useParams();
+
+  const [selectedCoin, setSelectedCoin] = useState(null);
+
+  // useRef -> variable that will hold its value between renders
+  let isFirstRender = useRef(true);
+  useEffect(() => {
+    console.log("useEffect running");
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      console.log("calling API");
+    }
+  }, []);
+
+  return (
+    <div className="page-container">
+      <h1>{symbol}</h1>
+    </div>
+  );
 };
 
 export default Coin;
--- src/pages/Currencies/index.js
+++ src/pages/Currencies/index.js
@@ -1,7 +1,40 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
 
 const Currencies = () => {
-  return <div className="page-container">Currencies</div>;
+  const navigate = useNavigate();
+
+  // eslint-disable-next-line
+  const [currencies, setCurrencies] = useState([
+    { name: "Bitcoin", symbol: "BTC" },
+    { name: "Litecoin", symbol: "LTC" },
+    { name: "Ethereum", symbol: "ETH" },
+    { name: "Ethereum Classic", symbol: "ETC" },
+    { name: "Stellar Lumens", symbol: "XLM" },
+    { name: "Dash", symbol: "DASH" },
+    { name: "Ripple", symbol: "XRP" },
+    { name: "Zcash", symbol: "ZEC" },
+  ]);
+
+  const handleClick = (coinSymbol) => {
+    navigate(`/coin/${coinSymbol}`);
+  };
+
+  let coinList = currencies.map((coin) => {
+    return (
+      <li key={JSON.stringify(coin)} onClick={() => handleClick(coin.symbol)}>
+        {coin.name}
+      </li>
+    );
+  });
+
+  return (
+    <div className="page-container">
+      <h1>Currencies Page</h1>
+      <ul id="coin-ul">{coinList}</ul>
+    </div>
+  );
 };
 
 export default Currencies;
